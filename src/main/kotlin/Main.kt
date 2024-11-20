ab55755cb9b4000dcd7a6524ccdd211d3309f948
barte45 <barte1234@gmail.com>
2024-11-20 14:26:06 +0100

    dodane okno z przyciskiem zamknij

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,21 @@
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.layout.StackPane
+import javafx.stage.Stage
+
+class TheaterBooking : Application() {
+    override fun start(stage: Stage) {
+        val closeButton = Button("Zamknij").apply {
+            setOnAction { stage.close() }
+        }
+
+        stage.title = "TheaterBooking"
+        stage.scene = Scene(StackPane(closeButton), 300.0, 300.0)
+        stage.show()
+    }
+}
+
 fun main() {
-    println("Hello ")
+    Application.launch(TheaterBooking::class.java)
 }
